04cdf0b0bb2c2328515173a0bb1243c30b7e8755
benjaminmontellimard <benjamin.m@coqpit.fr>
2024-04-26 11:13:47 +0200

    Added computed value

--- src/views/HomeNewView.vue
+++ src/views/HomeNewView.vue
@@ -1,18 +1,23 @@
 <script setup>
-import {ref, reactive} from "vue";
+import {ref, reactive, computed} from "vue";
 
 const h1desc = "Hello";
 const inputval = ref("val");
 const inputvalnb = ref(12);
 
 const user = reactive({
-  name: "name",
+  name: "Name",
   age: 18,
 });
 
 const process = (e) => {
   console.log(e.target.name+" changed with val: "+e.target.value);
 };
+
+const sentence = computed(() => {
+  const years = (user.age > 1) ? "years" : "year";
+  return "Hello, I'm "+user.name+" & I'm "+user.age+" "+years+" old.";
+});
 </script>
 
 <template>
@@ -28,13 +33,31 @@ const process = (e) => {
     <hr>
 
     <input name="name" type="text" v-model="user.name" v-on:change="process">
-    <p>Name : {{ user.name }}</p>
+    <p class="bold"
+       :class="{'adult':user.age >= 18,'child':user.age <= 10,}"
+    >{{ user.name }}</p>
 
     <input name="age" type="number" v-model="user.age" v-on:change="process">
-    <p>Age : {{ user.age }}</p>
+    <p class="bold"
+       :class="{'adult':user.age >= 18,'child':user.age <= 10,}"
+    >Age : {{ user.age }}</p>
 
-    <div v-if="user.age >= 18">User adult</div>
+    <div v-if="user.age >= 18" class="adult">User adult</div>
     <div v-else-if="user.age > 10">User teenager</div>
-    <div v-else>User child</div>
+    <div v-else class="child">User child</div>
+
+    <p>{{ sentence }}</p>
   </main>
 </template>
+
+<style scoped>
+  .bold{
+    font-weight: bold;
+  }
+  .adult{
+    color: deepskyblue;
+  }
+  .child{
+    color: pink;
+  }
+</style>
